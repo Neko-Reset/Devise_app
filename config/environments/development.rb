59f02cd588dda0965e3de32fe37cd789edebfed3
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-09-18 09:26:39 +0900

    メール認証完成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,16 +15,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # メールアドレス認証
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :user_name => 'Gメールアドレス',
-    :password => 'Gメールパスワード',
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => 'smtp.gmail.com',
+  #   :port => 587,
+  #   :user_name => '自分のメアド',
+  #   :password => '作成したパスワード',
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 
   # Show full error reports.
   config.consider_all_requests_local = true
